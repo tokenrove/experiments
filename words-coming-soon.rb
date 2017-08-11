206b3a17127f579e144620b2f4acf6f8ba8073ed
Julian Squires <julian@cipht.net>
2017-08-11 19:22:37 -0400

    Include prefixes in timestamp words

--- words-coming-soon.rb
+++ words-coming-soon.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
+# Base-36 timestamp words usable in the next year or so.
 
 ts = {}
 open('/usr/share/dict/words') do |f|
   f.each_line do |l|
-    t = Time.at(l.downcase.tr('^a-z','').to_i(36))
+    t = Time.at(l.downcase.tr('^a-z','').ljust(6,'0').to_i(36))
     ts[t] = l if (0..365).include?((t-Time.now) / 86400)
   end
 end
 
-ts.sort.each { |k,v| puts "#{k} #{v}" }
+ts.sort.each { |k,v| puts "#{k} #{v.chomp.ljust(6,'.')}" }
